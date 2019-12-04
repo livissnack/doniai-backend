4674fc9551873b0b37b2caa3ee3bb274b7252c3d
guan xin <guanxin@tinytiger.cn>
2019-12-04 16:39:54 +0800

    修改文件编码格式

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  publicPath: "/",
+  publicPath: '/',
   devServer: {
     proxy: {
-      "/api": {
-        target: "https://api.doniai.com/",
+      '/api': {
+        target: 'https://api.doniai.com/',
         changeOrigin: true,
         ws: true,
         pathRewrite: {
-          "^/api": "/"
+          '^/api': '/'
         }
       }
     }
@@ -17,16 +17,16 @@ module.exports = {
   configureWebpack: {
     //警告 webpack 的性能提示
     performance: {
-      hints: "warning",
+      hints: 'warning',
       //入口起点的最大体积
       maxEntrypointSize: 50000000,
       //生成文件的最大体积
       maxAssetSize: 30000000,
       //只给出 js 文件的性能提示
       assetFilter: function(assetFilename) {
-        return assetFilename.endsWith(".js");
+        return assetFilename.endsWith('.js')
       }
     }
   },
   productionSourceMap: false
-};
+}
